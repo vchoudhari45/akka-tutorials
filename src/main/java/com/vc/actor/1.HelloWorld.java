adc587618e2bf762ba2287812f756768d2f01940
Vishal Choudhari <vishal.choudhari2011@gmail.com>
2022-01-28 09:53:16 +0530

    Hello World

---
+++ src/main/java/com/vc/actor/1.HelloWorld.java
@@ -0,0 +1,85 @@
+package com.vc.actor;
+
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.ActorSystem;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.AbstractBehavior;
+import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.actor.typed.javadsl.Receive;
+
+class GreeterActor extends AbstractBehavior<GreeterActor.Command> {
+  protected interface Command{}
+
+  public static final class ThankYou implements GreeterActor.Command {
+    final ActorRef<GreetedActor.Command> from;
+    public ThankYou(ActorRef<GreetedActor.Command> from) {
+      this.from = from;
+    }
+  }
+
+  static Behavior<GreeterActor.Command> create() {
+    return Behaviors.setup(context -> new GreeterActor(context));
+  }
+
+  private GreeterActor(ActorContext<GreeterActor.Command> context) {
+    super(context);
+
+    ActorRef<GreetedActor.Command> actorRef
+     = getContext().spawn(GreetedActor.create(), "GreetedActor");
+
+    actorRef.tell(new GreetedActor.Greetings("Greetings", getContext().getSelf()));
+  }
+
+  @Override
+  public Receive<Command> createReceive() {
+    return newReceiveBuilder()
+     .onMessage(ThankYou.class, thankYou -> onThankYou(thankYou))
+     .build();
+  }
+
+  private Behavior<GreeterActor.Command> onThankYou(ThankYou thankYou) {
+    getContext().getLog().info("Thank you received from Greeted actor {}", thankYou.from);
+    return this;
+  }
+}
+
+class GreetedActor extends AbstractBehavior<GreetedActor.Command> {
+  protected interface Command{}
+
+  public static final class Greetings implements GreetedActor.Command {
+    final String msg;
+    final ActorRef<GreeterActor.Command> from;
+    public Greetings(String msg, ActorRef<GreeterActor.Command> from) {
+      this.msg = msg;
+      this.from = from;
+    }
+  }
+
+  static Behavior<GreetedActor.Command> create() {
+    return Behaviors.setup(context -> new GreetedActor(context));
+  }
+
+  private GreetedActor(ActorContext<GreetedActor.Command> context) {
+    super(context);
+  }
+
+  @Override
+  public Receive<Command> createReceive() {
+    return newReceiveBuilder()
+     .onMessage(Greetings.class, greetings -> onGreetings(greetings))
+     .build();
+  }
+
+  private Behavior<GreetedActor.Command> onGreetings(Greetings greetings) {
+    getContext().getLog().info("{} received from Greeting actor {}", greetings.msg, greetings.from);
+    greetings.from.tell(new GreeterActor.ThankYou(getContext().getSelf()));
+    return this;
+  }
+}
+
+class HelloWorld {
+  public static void main(String[] args) {
+    ActorSystem.create(GreeterActor.create(), "actorSystem");
+  }
+}
---
+++ src/main/java/com/vc/actor/2.StoppingActor.java
@@ -0,0 +1,96 @@
+package com.vc.actor;
+
+import akka.actor.typed.ActorSystem;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.PostStop;
+import akka.actor.typed.javadsl.AbstractBehavior;
+import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.actor.typed.javadsl.Receive;
+
+
+class MasterControlProgram extends AbstractBehavior<MasterControlProgram.Command> {
+  protected interface Command {}
+
+  public static final class SpawnJob implements Command {
+    public final String name;
+
+    public SpawnJob(String name) {
+      this.name = name;
+    }
+  }
+
+  public enum GracefulShutdown implements Command {
+    INSTANCE
+  }
+
+  public static Behavior<Command> create() {
+    return Behaviors.setup(MasterControlProgram::new);
+  }
+
+  public MasterControlProgram(ActorContext<Command> context) {
+    super(context);
+  }
+
+  @Override
+  public Receive<Command> createReceive() {
+    return newReceiveBuilder()
+      .onMessage(SpawnJob.class, this::onSpawnJob)
+      .onMessage(GracefulShutdown.class, message -> onGracefulShutdown())
+      .onSignal(PostStop.class, signal -> onPostStop())
+      .build();
+  }
+
+  private Behavior<Command> onSpawnJob(SpawnJob message) {
+    getContext().getSystem().log().info("Spawning job {}!", message.name);
+    getContext().spawn(Job.create(message.name), message.name);
+    return this;
+  }
+
+  private Behavior<Command> onGracefulShutdown() {
+    getContext().getSystem().log().info("Initiating graceful shutdown...");
+    return Behaviors.stopped();
+  }
+
+  private Behavior<Command> onPostStop() {
+    getContext().getSystem().log().info("Master Control Program stopped");
+    return this;
+  }
+}
+
+class Job extends AbstractBehavior<Job.Command> {
+  protected interface Command {}
+
+  public static Behavior<Command> create(String name) {
+    return Behaviors.setup(context -> new Job(context, name));
+  }
+
+  private final String name;
+
+  public Job(ActorContext<Command> context, String name) {
+    super(context);
+    this.name = name;
+  }
+
+  @Override
+  public Receive<Job.Command> createReceive() {
+    return newReceiveBuilder()
+      .onSignal(PostStop.class, postStop -> onPostStop()).build();
+  }
+
+  private Behavior<Command> onPostStop() {
+    getContext().getSystem().log().info("Worker {} stopped", name);
+    return this;
+  }
+}
+
+class StoppingActor {
+  public static void main(String[] args) {
+    ActorSystem<MasterControlProgram.Command> actorSystem
+      = ActorSystem.create(MasterControlProgram.create(), "MasterProgram");
+
+    actorSystem.tell(new MasterControlProgram.SpawnJob("Job-1"));
+    actorSystem.tell(new MasterControlProgram.SpawnJob("Job-2"));
+    actorSystem.tell(MasterControlProgram.GracefulShutdown.INSTANCE);
+  }
+}
